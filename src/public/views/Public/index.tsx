840b21fe01c57a6c9d959a8b99a8e84b551aa126
youzetsu <dotochkamy@gmail.com>
2022-02-15 00:21:22 +0300

    fix(core): fix after review

--- src/public/views/Public/index.tsx
+++ src/public/views/Public/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { UiSelect } from '../../../core/components'
 
 const Public = () => {
   return (
     <div>
-      <UiSelect selectItems={[ '1', '2', '3', '4', '5', '6', '7', '8', '9' ]} onClickItem={() => console.log(1)} />
+      <h1>123</h1>
     </div>
   )
 }
