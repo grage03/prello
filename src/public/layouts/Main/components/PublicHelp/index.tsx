3a2404d8ad389c2d6a89d28fac2fbfd7bebeb276
grage03 <alex.grage@yandex.by>
2022-02-20 20:18:33 +0300

    start(public): start work with first block on public page

---
+++ src/public/layouts/Main/components/PublicHelp/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import './style/styles.sass'
+import { UiImage } from '../../../../../core/components/ui-components'
+import { UiInput } from '../../../../../core/components/ui-components/Input'
+
+export const PublicHelp = () => {
+  return (
+    <div className="help-teams">
+      <div className="help-teams__interaction">
+        <h1 className="help-teams__title">We help teams to be more effective</h1>
+        <p className="help-teams__description">
+          A complete set of tools for your work: Kanban boards, weekly calendar, task management, flexible
+          notifications, and more.
+        </p>
+        <div className="help-teams__launch">
+          <UiInput
+            placeholder="Enter your email address"
+            type="email"
+            width="80%"
+            isButton
+          />
+        </div>
+      </div>
+      <div className="help-teams__image">
+        <UiImage src="helpTeams" width="480" />
+      </div>
+    </div>
+  )
+}
--- src/public/layouts/Main/index.tsx
+++ src/public/layouts/Main/index.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 import '../../../assets/styles/_helpers.sass'
 
+import { PublicHelp } from './components/PublicHelp'
+
 export const PublicPage = () => {
   return (
     <div className="h-container">
-      test
+      <PublicHelp />
     </div>
   )
 }
