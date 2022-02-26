bd386c065cd04e0324e99dd187ce0276b205f987
grage03 <alex.grage@yandex.by>
2022-02-26 16:22:37 +0300

    start(public): end work on public score

--- src/public/layouts/Main/index.tsx
+++ src/public/layouts/Main/index.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 import '../../../assets/styles/_helpers.sass'
 
-import { PublicHelp, PublicReviews } from './components'
+import { PublicAssessment, PublicHelp, PublicReviews } from './components'
 
 export const PublicPage = () => {
   return (
     <div className="h-container">
       <PublicHelp />
       <PublicReviews />
+      <PublicAssessment />
     </div>
   )
 }
