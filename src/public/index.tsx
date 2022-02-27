fb956b744305dac30eb164df936d504d9d9aece8
grage03 <alex.grage@yandex.by>
2022-02-27 21:53:46 +0300

    start(public): start footer

--- src/public/index.tsx
+++ src/public/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import { Header } from './components/Header'
+import { Header, Footer } from './components'
 import './style/styles.sass'
 
 const PublicRoute = () => {
@@ -8,6 +8,7 @@ const PublicRoute = () => {
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   )
 }
