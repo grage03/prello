de16874a1c450d0be7c32c814454280907dc859a
grage03 <alex.grage@yandex.by>
2022-03-05 20:44:10 +0300

    refactoring(public): rewrite page margin to gap with flex-column

--- src/public/layouts/Main/index.tsx
+++ src/public/layouts/Main/index.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
 
 import '../../../assets/styles/_helpers.sass'
+import './style/styles.sass'
 
 import {
   PublicAssessment,
   PublicHelp,
   PublicReviews,
   PublicReasons,
+  PublicPartners,
 } from './components'
 
 export const PublicPage = () => {
   return (
-    <main className="h-container">
+    <main className="h-container public-wrapper">
       <PublicHelp />
       <PublicReasons />
+      <PublicPartners />
       <PublicReviews />
       <PublicAssessment />
     </main>
