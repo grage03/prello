1598882401973331de93ca2e7f39c35487b39803
grage03 <alex.grage@yandex.by>
2022-03-05 19:41:32 +0300

    start(public): add block (reasons)

---
+++ src/public/layouts/Main/components/PublicReasons/enum/index.ts
@@ -0,0 +1,26 @@
+export const reasons = [
+  {
+    title: "Download Templates",
+    description: "Muffin lemon drops toffee. Pudding tootsie roll brownie jelly beans.",
+    to: "",
+    img: "downloadTemplates",
+  },
+  {
+    title: "Work from Home",
+    description: "Muffin lemon drops toffee. Pudding tootsie roll brownie jelly beans.",
+    to: "",
+    img: "workHome",
+  },
+  {
+    title: "Collaborate with Teams",
+    description: "Muffin lemon drops toffee. Pudding tootsie roll brownie jelly beans.",
+    to: "",
+    img: "collaborateTeams",
+  },
+  {
+    title: "Backup Everything",
+    description: "Muffin lemon drops toffee. Pudding tootsie roll brownie jelly beans.",
+    to: "",
+    img: "backupEverything",
+  },
+]
--- src/public/layouts/Main/index.tsx
+++ src/public/layouts/Main/index.tsx
@@ -2,12 +2,18 @@ import React from 'react'
 
 import '../../../assets/styles/_helpers.sass'
 
-import { PublicAssessment, PublicHelp, PublicReviews } from './components'
+import {
+  PublicAssessment,
+  PublicHelp,
+  PublicReviews,
+  PublicReasons,
+} from './components'
 
 export const PublicPage = () => {
   return (
     <main className="h-container">
       <PublicHelp />
+      <PublicReasons />
       <PublicReviews />
       <PublicAssessment />
     </main>
