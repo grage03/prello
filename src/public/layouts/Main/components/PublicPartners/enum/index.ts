30087bf4515953a003b6e2d284c06222522d1b16
grage03 <alex.grage@yandex.by>
2022-03-05 20:40:54 +0300

    start(public): add block (partners)

---
+++ src/public/layouts/Main/components/PublicPartners/enum/index.ts
@@ -0,0 +1,18 @@
+export const partners = [
+  {
+    img: "coupang",
+    to: null,
+  },
+  {
+    img: "cgv",
+    to: null,
+  },
+  {
+    img: "naver",
+    to: null,
+  },
+  {
+    img: "whale",
+    to: null,
+  },
+]
