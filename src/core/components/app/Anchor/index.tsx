76e158a1cc8c075bad01daccb422972167812bb9
grage03 <alex.grage@yandex.by>
2022-04-14 22:14:51 +0300

    add(core): add style to anchor

--- src/core/components/app/Anchor/index.tsx
+++ src/core/components/app/Anchor/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { AnchorProps } from './interface'
 
+import './style/styles.sass'
+
 // TODO: the header prevents you from scrolling through the block, need to add padding with the length of the header
 export const Anchor = ({ children, anchor }: AnchorProps) => {
   return (
-    <a href={`#${anchor}`}>
+    <a href={`#${anchor}`} className="anchor">
       {children}
     </a>
   )
