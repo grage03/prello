cd4fc17638aa2905b4fb9d9b20f29e6241fc18ef
grage03 <alex.grage@yandex.by>
2022-04-14 22:13:38 +0300

    start(public): rewrote the burger menu

--- src/public/components/Header/enums/index.ts
+++ src/public/components/Header/enums/index.ts
@@ -1,5 +1,10 @@
+// TODO: add index.ts import
+import { Reasons } from '../../../../assets/icon/app/burgerMenu/reasons'
+import { Partners } from '../../../../assets/icon/app/burgerMenu/partners'
+import { Reviews } from '../../../../assets/icon/app/burgerMenu/reviews'
+
 export const navigationOptions = [
-  { name: 'public-header-reasons', option: 'reasons' },
-  { name: 'public-header-partners', option: 'partners' },
-  { name: 'public-header-reviews', option: 'reviews' },
+  { name: 'public-header-reasons', option: 'reasons', icon: Reasons },
+  { name: 'public-header-partners', option: 'partners', icon: Partners },
+  { name: 'public-header-reviews', option: 'reviews', icon: Reviews },
 ]
