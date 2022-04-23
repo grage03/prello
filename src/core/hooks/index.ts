a1f3fa8ffb8b10771884c7e50b0cc29cf9c1e9d7
grage03 <alex.grage@yandex.by>
2022-04-23 18:06:09 +0300

    add(app): small change

---
+++ src/core/hooks/index.ts
@@ -0,0 +1,15 @@
+import { useFetch } from './useFetch/useFetch'
+import { usePosition } from './usePosition/usePosition'
+import { useScroll } from './useScroll/useScroll'
+import { useSlots } from './useSlots/useSlots'
+import { useStorage } from './useStorage/useStorage'
+import { useDispatch } from './useDispatch/useDispatch'
+
+export {
+  usePosition,
+  useDispatch,
+  useStorage,
+  useScroll,
+  useSlots,
+  useFetch,
+}
