67e1dfadae914a798645aad28c22bf78aa59ba1b
grage03 <alex.grage@yandex.by>
2022-05-12 23:26:19 +0300

    delete(core): delete useStorage

--- src/core/hooks/index.ts
+++ src/core/hooks/index.ts
@@ -2,13 +2,11 @@ import { useFetch } from './useFetch/useFetch'
 import { usePosition } from './usePosition/usePosition'
 import { useScroll } from './useScroll/useScroll'
 import { useSlots } from './useSlots/useSlots'
-import { useStorage } from './useStorage/useStorage'
 import { useDispatch } from './useDispatch/useDispatch'
 
 export {
   usePosition,
   useDispatch,
-  useStorage,
   useScroll,
   useSlots,
   useFetch,
