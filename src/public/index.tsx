b692485eafb90bbde3ea02639ecb656a5e4344de
grage03 <alex.grage@yandex.by>
2022-05-23 22:27:08 +0300

    add(public): add fix for router

--- src/public/index.tsx
+++ src/public/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import { Header, Footer } from './components'
 import { Layout } from '../core/components/app/Layout'
+import { LoginPage, RegistrationPage } from './modules/login/views'
+import { PublicPage } from './layouts/Main'
 
 const PublicRoute = () => {
   return (
     <Layout>
       <>
         <Header />
-        <Outlet />
+        <Routes>
+          <Route index element={<PublicPage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="registration" element={<RegistrationPage />} />
+        </Routes>
         <Footer />
       </>
     </Layout>
