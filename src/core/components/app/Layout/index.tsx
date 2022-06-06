60861e2379358bfbb34fd1610cbe0b5176897319
grage03 <alex.grage@yandex.by>
2022-06-06 21:29:39 +0300

    fix(app): edit context prop

--- src/core/components/app/Layout/index.tsx
+++ src/core/components/app/Layout/index.tsx
@@ -1,7 +1,7 @@
 import React, { useMemo } from 'react'
 import { ILayoutProps } from './interface'
 
-export const Layout = ({ children, contextStore, ContextProvider }: ILayoutProps) => {
+export const Layout = ({ children, contextStore, contextProvider: ContextProvider }: ILayoutProps) => {
   const store = useMemo(() => (contextStore), [])
   return (
     <ContextProvider.Provider value={store}>
