0ed72a1428b6df4250a411b2f1c995e2ef2da4c4
grage03 <alex.grage@yandex.by>
2022-06-12 09:19:11 +0300

    start(app): add cypress-bot

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from "cypress"
 
 export default defineConfig({
+  projectId: 'k69hug',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-});
+})
