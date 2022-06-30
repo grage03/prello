f10cbb0c542b01e97b64846052e4c05b4b7b5cfe
IzzXr <90339036+IzzXr@users.noreply.github.com>
2022-06-30 22:02:57 +0300

    added useMemo

--- src/core/components/ui-components/FormGroup/components/Error/index.tsx
+++ src/core/components/ui-components/FormGroup/components/Error/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 import { IError } from './interface'
 
@@ -16,15 +16,15 @@ type ErrorMessageType = {
 export const Error = ({ error }: IError) => {
   const { t } = useTranslation()
 
-  const getErrorMessage = (type: string) => {
+  const message = useMemo(() => {
     const errorMessage: ErrorMessageType = {
       [TYPES.REQUIRED]: t('translation:required-field'),
       [TYPES.IS_VALUE]: t('translation:is_value-field'),
     }
 
-    return errorMessage[type]
-  }
+    return errorMessage[error?.type as string]
+  }, [ error, t ])
 
   if (!error) return null
-  return <label htmlFor={error.message} className={styles['error__text']}>{getErrorMessage(error.type)}</label>
+  return <label htmlFor={error.message} className={styles['error__text']}>{message}</label>
 }
