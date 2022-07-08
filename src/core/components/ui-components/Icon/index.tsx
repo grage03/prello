629083bdf5c03da3bde878388ccfbff3b0be4e66
grage03 <alex.grage@yandex.by>
2022-07-08 22:24:20 +0300

    start(app): added new props to icon

--- src/core/components/ui-components/Icon/index.tsx
+++ src/core/components/ui-components/Icon/index.tsx
@@ -2,11 +2,19 @@ import React from 'react'
 import { IIconProps } from './interface'
 
 export const Icon = ({
-  viewBox = "0 0 24 24", icon, size = 24, fill = 'fillColor',
+  viewBox = "0 0 24 24",
+  icon,
+  size = 24,
+  fill = 'fillColor',
+  onClick,
+  className,
 }: IIconProps) => {
+  const handleClick = () => onClick && onClick()
+
   return (
     <svg
       width={size}
+      className={className}
       height={size}
       xmlns="http://www.w3.org/2000/svg"
       viewBox={viewBox}
@@ -14,6 +22,7 @@ export const Icon = ({
       stroke="strokeColor"
       aria-hidden="true"
       role="presentation"
+      onClick={handleClick}
       style={{
         display: 'flex',
         justifyContent: 'center',
--- src/core/components/ui-components/Icon/interface/index.ts
+++ src/core/components/ui-components/Icon/interface/index.ts
@@ -1,8 +1,10 @@
 import { ReactElement } from 'react'
 
 export interface IIconProps {
+  className?: string | undefined
   fill?: string
   icon: ReactElement
+  onClick?: () => void
   size?: number
   viewBox?: string
 }
